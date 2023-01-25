37708df88c223b45182c0cb032a09deb7e5a4494
Olga Buditska <olgabudickaja@gmail.com>
2023-01-26 00:42:50 +0200

    update token

--- src/RequestMethods.js
+++ src/RequestMethods.js
@@ -7,7 +7,7 @@ localStorage.setItem('REACT_APP_ACCESS_TOKEN', token)
 const TOKEN = localStorage.getItem('REACT_APP_ACCESS_TOKEN')
 
 export const publicRequest = axios.create({
-    baseURL: BASE_URL,
+    baseURL: BASE_URL
 });
 export const userRequest = axios.create({
     baseUrl: BASE_URL,
