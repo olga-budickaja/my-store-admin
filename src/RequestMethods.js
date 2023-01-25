f7a0ee7d115015e6ccd5877e06a434fa12e1c247
Olga Buditska <olgabudickaja@gmail.com>
2023-01-26 00:59:42 +0200

    update token

--- src/RequestMethods.js
+++ src/RequestMethods.js
@@ -5,7 +5,7 @@ const token= process.env.REACT_APP_ACCESS_TOKEN;
 localStorage.setItem('REACT_APP_ACCESS_TOKEN', token)
 
 const TOKEN = localStorage.getItem('REACT_APP_ACCESS_TOKEN')
-
+console.log(TOKEN)
 export const publicRequest = axios.create({
     baseURL: BASE_URL
 });
