428c1b431778177a1290a8b681302fdb40190f70
Olga Buditska <olgabudickaja@gmail.com>
2023-01-25 21:22:24 +0200

    update token

--- src/RequestMethods.js
+++ src/RequestMethods.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const BASE_URL = "https://boa-store.herokuapp.com/api/";
 const token= process.env.REACT_APP_ACESS_TOKEN;
-localStorage.setItem('REACT_APP_TOKEN', token)
+localStorage.setItem('REACT_APP_ACCESS_TOKEN', token)
 
-const TOKEN = localStorage.getItem('ACCESS_TOKEN')
+const TOKEN = localStorage.getItem('REACT_APP_ACCESS_TOKEN')
 
 export const publicRequest = axios.create({
     baseUrl: BASE_URL,
